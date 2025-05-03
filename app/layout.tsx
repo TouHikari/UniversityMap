5bed1e81c8417312b2c71440dd60ae87bddf7c8b
TouHikari <115980406+TouHikari@users.noreply.github.com>
2025-05-03 19:56:42 +0800

    :bug: fix: 修复触屏设备的缩放问题 (#1)
    * fix(layout): configure viewport for mobile devices
    
    * :art: chore: delete something
    
    ---------
    
    Co-authored-by: zhxy-CN <admin@owo.cab>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
 
 export const metadata: Metadata = {
   title: "University Map",
@@ -15,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh">
       <body className={inter.className}>{children}</body>
     </html>
   );
